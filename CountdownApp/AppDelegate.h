8a14d30011a22b67e1245b7f1aef361e25080d42
Joe Cortopassi <joe@joecortopassi.com>
2013-01-18 22:16:11 -0800

    Initial Commit

---
+++ CountdownApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CountdownApp
+//
+//  Created by Joe Cortopassi on 1/18/13.
+//  Copyright (c) 2013 Joe Cortopassi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CountdownApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CountdownApp
+//
+//  Created by Joe Cortopassi on 1/18/13.
+//  Copyright (c) 2013 Joe Cortopassi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
